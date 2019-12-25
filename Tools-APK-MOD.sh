7814e75875208cd61307ff43819695a4151d25ae
TeamTCN <falwan0206@gmail.com>
2019-12-25 11:13:48 +0700

    Update Tools-APK-MOD.sh

--- Tools-APK-MOD.sh
+++ Tools-APK-MOD.sh
@@ -1,78 +1,105 @@
-#!/bin/bash
+#!/bin/bash                                                       
 
 clear
-echo "\033[1;34m      ╔═════════════════════════════════════════════════════════════╗"
+echo "\033[1;34m      ╔═══════════════════════════════════════════╗
 toilet -f future "  Tools Install APK MOD" -F gay
-echo "\033[1;34m      ╚═════════════════════════════════════════════════════════════╝"
+echo "\033[1;34m      ╚═══════════════════════════════════════════╝
 sleep 2
 toilet -f Roman "Tools-A" -F gay
 sleep 2
-echo "\033[1;34m╔═════════════════════════════════════╗"
-echo "\033[1;32m (1)Install Bahan Bahan"
-echo "\033[1;31m<═════════════════════════════════════>"
+echo "\033[1;34m╔═══════════════════════════════════════════╗"
+echo "\033[1;32m (1)Install Bahan Bahan"                          
+echo "\033[1;31m<══════════════════════════════════════════>"
 echo "\033[1;32m (2)Tools Download COC MOD V1"
-echo "\033[1;31m<═════════════════════════════════════>"
-echo "\033[1;32m (3)Tools Download COC MOD V2"
-echo "\033[1;31m<═════════════════════════════════════>"
-echo "\033[1;32m (4)Tools Download COC MOD v3"
-echo "\033[1;31m<═════════════════════════════════════>"
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (3)Tools Download COC MOD V2"                    
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (4)Tools Download COC MOD v3"                    
+echo "\033[1;31m<══════════════════════════════════════════>"     
 echo "\033[1;32m (5)Tools Download COC MOD V4"
-echo "\033[1;31m<═════════════════════════════════════>"
+echo "\033[1;31m<══════════════════════════════════════════>"
 echo "\033[1;32m (6)Tools Download COC MOD V5"
-echo "\033[1;31m<═════════════════════════════════════>"
+echo "\033[1;31m<══════════════════════════════════════════>"
 echo "\033[1;32m (7)Tools Download COC MOD V6"
-echo "\033[1;31m<═════════════════════════════════════>"
+echo "\033[1;31m<══════════════════════════════════════════>"
 echo "\033[1;32m (8)Tools Download FREE FIRE MOD"
-echo "\033[1;31m<═════════════════════════════════════>"
+echo "\033[1;31m<══════════════════════════════════════════>"
 echo "\033[1;32m (9)Tools Download Mobile Legend MOD"
-echo "\033[1;31m<═════════════════════════════════════>"
+echo "\033[1;31m<══════════════════════════════════════════>"
 echo "\033[1;32m (10)Tools Download Mobile Legend MOD V2"
-echo "\033[1;31m<═════════════════════════════════════>"
+echo "\033[1;31m<══════════════════════════════════════════>"
 echo "\033[1;32m (11)Tools Download Kwhatsapp Imuni"
-echo "\033[1;31m<═════════════════════════════════════>"
+echo "\033[1;31m<══════════════════════════════════════════>"
 echo "\033[1;32m (12lTools Download Kwhatsapp Imuni V3"
-echo "\033[1;31m<═════════════════════════════════════>"
+echo "\033[1;31m<══════════════════════════════════════════>"
 echo "\033[1;32m (13)Tools Download Kwhatsapp Imuni V4"
-echo "\033[1;31m<═════════════════════════════════════>"
+echo "\033[1;31m<══════════════════════════════════════════>"
 echo "\033[1;32m (14)Tools Download Wa MOD"
-echo "\033[1;31m<═════════════════════════════════════>"
+echo "\033[1;31m<══════════════════════════════════════════>"
 echo "\033[1;32m (15)Tools Download Kwhatsapp Imuni Lord"
-echo "\033[1;31m<═════════════════════════════════════>"
+echo "\033[1;31m<══════════════════════════════════════════>"
 echo "\033[1;32m (16)Tools Download Wa Royal Blue"
-echo "\033[1;31m<═════════════════════════════════════>"
-echo "\033[1;32m (17)
-echo "\033[1;31m<═════════════════════════════════════>"
-echo "\033[1;32m (18)
-echo "\033[1;31m<═════════════════════════════════════>"
-echo "\033[1;32m (19)
-echo "\033[1;31m<═════════════════════════════════════>"
-echo "\033[1;32m (20)
-echo "\033[1;31m<═════════════════════════════════════>"
-echo "\033[1;32m (21)
-echo "\033[1;31m<═════════════════════════════════════>"
-echo "\033[1;32m (22)
-echo "\033[1;31m<═════════════════════════════════════>"
-echo "\033[1;32m (23)
-echo "\033[1;31m<═════════════════════════════════════>"
-echo "\033[1;32m (24)
-echo "\033[1;31m<═════════════════════════════════════>"
-echo "\033[1;32m (25)
-echo "\033[1;31m<═════════════════════════════════════>"
-echo "\033[1;32m (26)
-echo "\[1;31m<═════════════════════════════════════>"
-echo "\033[1;32m (27)
-echo "\033[1;31m<═════════════════════════════════════>"
-echo "\033[1;32m (28)
-echo "\033[1;31m<═════════════════════════════════════>"
-echo "\033[1;32m (29)
-echo "\033[1;31m<═════════════════════════════════════>"
-echo "\033[1;32m (30)
-echo "\033[1;31m<═════════════════════════════════════>"
-echo "\033[1;32m (31)
-echo "\033[1;31m<═════════════════════════════════════>"
-echo "\033[1;32m (32)
-echo "\033[1;34m╚══════════════════════════════════════╝"
-read -p "Masukan Pilihan Anda Disini :" pilih;
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (17)Tools Download Wa Devil V14"                 
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (18)Tools Download Wa Devil V12"
+echo "\033[1;31m<══════════════════════════════════════════>"     
+echo "\033[1;32m (19)Tools Download Wa Devil V11"
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (20)Tools Download Wa Devil V10"                 
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (21)Tools Download Wa Devil V9"
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (22)Tools Download Wa Devil V8"
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (23)Tools Download Wa Devil V7"
+echo "\033[1;31m<══════════════════════════════════════════>"     
+echo "\033[1;32m (24)Tools Download Wa Devil V6"
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (25)Tools Download Wa Devil V5"
+echo "\033[1;31m<══════════════════════════════════════════>"     
+echo "\033[1;32m (26)Tools Download Wa CDN V2.17.250"
+echo "\033[1;31m<══════════════════════════════════════════>"     
+echo "\033[1;32m (27)Tools Download Wa Nyai"                      
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (28)Tools Download Wa STCT"
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (29)Tools Download Wa GBIOS2 V7"
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (30)Tools Download Wa Seketaris"
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (31)Tools Download Wa YOWA SamMods V8"
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (32)Tools Download Wa Aero Hazar V8"
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (33)Tools Download Wa SAMWA-B V4"
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (34)Tools Download Wa FM-WA V8"
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (35)Tools Download Wa TKWhatsapp"                
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (36)Tools Download Wa Lori V1.0"                 
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (37)Tools Download Wa Aero Hazar V8.5"
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (38)Tools Download Wa Aero Hazar V8.11"          
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (39)Tools Download Wa Kwhatsapp V8"              
+echo "\033[1;31m<══════════════════════════════════════════>"    
+echo "\033[1;32m (40)Tools Download Wa Rifki Official"
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (41)Tools Download Wa KAWA V8.15 OFC"
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (42)Tools Download Wa KAWA V8.15 IMUN"
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (43)Tools Download APK COC MOD V2.11"
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (44)Kembali Ke Menu Tools Installer"
+echo "\033[1;31m<══════════════════════════════════════════>"
+echo "\033[1;32m (45)Keluar Dari Tools Ini"
+echo "\033[1;34m╚═══════════════════════════════════════════╝"
+read -p "┌─[Pilih] ~ [×] ~  [TOOLS-APK-MOD] - [+] - [MR.ALF]
+└─[$] <>" pilih;
 if [ $pilih = "1" ]
 then
     clear
@@ -97,20 +124,13 @@ then
     pip install mechanize
     pip install requests
     pip install request
-    pip install bs4
-    pip install tqdm
-    pip install progress
-    clear
-    toilet -f future "Install Selesai..." -F gay
+    pip install bs4                                                                         pip install tqdm                                                                        pip install progress
+    clear                                                                                   toilet -f future "Install Selesai..." -F gay
     sleep 2
-    clear
-    sh Tools-APK-MOD.sh
-elif [ $pilih = "2" ]
-then
+    clear                                                                                   sh Tools-APK-MOD.sh                                                                 elif [ $pilih = "2" ]                                                                   then
      clear
      python2 COC-MOD.py
-elif [ $pilih = "3" ]
-then
+elif [ $pilih = "3" ]                                                                   then
      clear
      python2 COC-MODV2.py
 elif [ $pilih = "4" ]
@@ -121,20 +141,13 @@ elif [ $pilih = "5" ]
 then
      clear
      python2 COC-MODV4.py
-elif [ $pilih = "6" ]
+elif [ $pilih = "6" ]                                                                   then                                                                                         clear
+     python2 COC-MODV5.py                                                               elif [ $pilih = "7" ]
 then
-     clear
-     python2 COC-MODV5.py
-elif [ $pilih = "7" ]
-then
-     clear
-     python2 COC-MODV6.py
-elif [ $pilih = "8" ]
-then
+     clear                                                                                   python2 COC-MODV6.py                                                               elif [ $pilih = "8" ]                                                                   then
      clear
      python2 FREE-FIRE-MOD.py
-elif [ $pilih = "9" ]
-then
+elif [ $pilih = "9" ]                                                                   then
      clear
      python2 Mobile-Legend-MOD.py
 elif [ $pilih = "10" ]
@@ -165,6 +178,118 @@ elif [ $pilih = "16" ]
 then
      clear
      python2 Wa-Imun-RoyalBlue.py
+elif [ $pilih = "17" ]
+then
+     clear
+     python2 WA-Devil-V14.py
+elif [ $pilih = "18" ]
+then
+     clear
+     python2 WA-Devil-V12.py
+elif [ $pilih = "19" ]
+then
+     clear
+     python2 WA-Devil-V11.py
+elif [ $pilih = "20" ]
+then
+     clear
+     python2 WA-Devil-V10.py                                                            elif [ $pilih = "21" ]
+then
+     clear                                                                                   python2 WA-Devil-V9.py                                                             elif [ $pilih = "22" ]                                                                  then
+     clear
+     python2 WA-Devil-V8.py
+elif [ $pilih = "23" ]                                                                  then
+     clear
+     python2 WA-Devil-V7.py
+elif [ $pilih = "24" ]
+then
+     clear
+     python2 WA-Devil-V6.py
+elif [ $pilih = "25" ]
+then
+     clear
+     python2 WA-Devil-V5.py
+elif [ $pilih = "26" ]
+then
+     clear
+     python2 WA-CDN-V2.py
+elif [ $pilih = "27" ]
+then
+     clear                                                                                   python2 WA-Nyai.py
+elif [ $pilih = "28" ]
+then
+     clear
+     python2 WA-STCT.py
+elif [ $pilih = "29" ]
+then
+      clear
+      python2 WA-GBIOS.py
+elif [ $pilih = "30" ]
+then
+      clear
+      python2 WA-SEKETARIS.py
+elif [ $pilih = "31" ]
+then
+      clear
+      python2 WA-YOWA-V8.py
+elif [ $pilih = "32" ]
+then
+     clear
+     python2 Wa-Mod-Aero-V8.py
+elif [ $pilih = "33" ]
+then
+     clear
+     python2 WA-SAMWA-B-V4.py
+elif [ $pilih = "34" ]
+then
+     clear
+     python2 WA-FM-WA-V8.py
+elif [ $pilih = "35" ]
+then
+     clear
+     python2 WA-TKWhatsapp.py
+elif [ $pilih = "36" ]
+then
+     clear                                                                                   python2 WA-LORI-V1.py                                                              elif [ $pilih = "37" ]                                                                  then
+     clear
+     python2 WA-AERO-V8.5.py
+elif [ $pilih = "38" ]                                                                  then
+     clear
+     python2 WA-AERO-V8.11.py
+elif [ $pilih = "39" ]
+then
+     clear
+     python2 WA-KWHATSAPP-V8.py
+elif [ $pilih = "40" ]
+then
+     clear
+     python2 WA-Rifki-OFFICIAL.py
+elif [ $pilih = "41" ]
+then
+     clear
+     python2 WA-KAWA-V8.15-OFC.py
+elif [ $pilih = "42" ]
+then
+     clear                                                                                   python2 WA-KAWA-V8.15-IMUN.py
+elif [ $pilih = "43" ]
+then
+     clear                                                                                   python2 COC-MOD-V2.11.py
+elif [ $pilih = "44" ]
+then
+     clear
+     sh Tools-APK-MOD.sh
+elif [ $pilih = "45" ]
+then
+     clear
+     echo "\033[1;31m[+]Terima Kasih Sudah Memakai Tools Saya[+]"
+     sleep 2
+     echo "\033[1;32m  [+]Silahkan Pakai Kembali Tools Saya[+]"
+     sleep 2
+     echo "\033[1;34m      [+]Salam Dari Saya MR.ALF[+]"
+     sleep 2
+     echo "\033[1;35m     [+]Salam Dari Team Saya TCN[+]"
+     sleep 2
+     echo "\033[1;33m   [+]Jangan Lupa Bahagia Hari Ini[+]"
 else
      clear
      echo "\033[1;34m╔══════════════════════════════════════╗"
